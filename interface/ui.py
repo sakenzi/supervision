e58d0bd189f770d4ecb94b016dc8f1313f4a0a19
sakenzi <ermekulysaken8@gmail.com>
2025-03-15 19:59:29 +0600

    create username and code button

--- interface/ui.py
+++ interface/ui.py
@@ -1,15 +1,17 @@
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QMenu
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QMenu, QLineEdit, QApplication
 from PyQt6.QtGui import QIcon, QAction
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QSystemTrayIcon
 from information.system_info import SystemInfo
 from information.user_monitor import UserMonitor
+import time
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Информация о системе")
-        self.setGeometry(100, 100, 400, 200)
+        self.setWindowTitle("kӨz")
+        self.setGeometry(100, 100, 400, 300)
 
         self.sys_info = SystemInfo()
         self.user_monitor = UserMonitor()
@@ -22,13 +24,23 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.central_widget)
         self.layout = QVBoxLayout(self.central_widget)
 
+        self.username_label = QLabel("Имя пользователя:")
+        self.username_input = QLineEdit()
+        self.layout.addWidget(self.username_label)
+        self.layout.addWidget(self.username_input)
+
+        self.code_label = QLabel("Введите код:")
+        self.code_input = QLineEdit()
+        self.layout.addWidget(self.code_label)
+        self.layout.addWidget(self.code_input)
+
         self.start_button = QPushButton("Начать")
         self.start_button.clicked.connect(self.start_monitoring)
         self.layout.addWidget(self.start_button)
 
-        self.mac_label = QLabel("MAC-адрес: ")
-        self.ip_label = QLabel("IP-адрес: ")
-        self.pc_name_label = QLabel("Имя ПК: ")
+        self.mac_label = QLabel()
+        self.ip_label = QLabel()
+        self.pc_name_label = QLabel()
 
         self.layout.addWidget(self.mac_label)
         self.layout.addWidget(self.ip_label)
@@ -51,6 +63,11 @@ class MainWindow(QMainWindow):
         self.tray_icon.activated.connect(self.tray_clicked)
 
     def start_monitoring(self):
+        username = self.username_input.text()
+        code = self.code_input.text()
+        print(f"{time.ctime()}: Имя пользователя: {username}")
+        print(f"{time.ctime()}: Код: {code}")
+
         self.show_system_info()
         self.user_monitor.start()
         self.hide()
